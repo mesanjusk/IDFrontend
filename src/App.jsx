08491738f37a68001a04153b8a07062c55aa0819
mesanjusk <116157981+mesanjusk@users.noreply.github.com>
2025-05-07 18:59:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './components/Home';
-import Upload from './componennts/Upload';
+import Home from '.src/components/Home';
+import Upload from '.src/componennts/Upload';
 
 function App() {
   return (
