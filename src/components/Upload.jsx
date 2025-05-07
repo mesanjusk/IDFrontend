f5e03a47d714d77a63c8039aca79232891e36660
mesanjusk <116157981+mesanjusk@users.noreply.github.com>
2025-05-07 18:52:31 +0530

    Update Upload.jsx

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import axios from 'axios';
 
-export default function ImageUploadPage() {
+export default function Upload() {
   const [file, setFile] = useState(null);
   const [title, setTitle] = useState('');
 
