705a1e1dafb4edb4e4fc3181fe291eafc05fd541
mesanjusk <116157981+mesanjusk@users.noreply.github.com>
2025-05-09 15:51:36 +0530

    Update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -29,20 +29,6 @@ function Home() {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-full max-w-6xl">
   {images.map((img, i) => (
-    img.images.length > 0
-      ? img.images.map((imageName, index) => (
-          <div key={`${i}-${index}`} className="group relative overflow-hidden rounded-lg shadow-md cursor-pointer">
-            <img 
-              src={`https://idbackend-rf1u.onrender.com/uploads/${imageName}`} 
-              alt="Uploaded" 
-              className="w-full h-full object-cover transition-transform duration-300 transform group-hover:scale-110"
-            />
-            <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-50 transition-opacity duration-200 flex justify-center items-center">
-              <p className="text-white text-lg font-semibold">View Image</p>
-            </div>
-          </div>
-        ))
-      : img.url && (
           <div key={i} className="group relative overflow-hidden rounded-lg shadow-md cursor-pointer">
             <img 
               src={img.url} 
@@ -54,7 +40,7 @@ function Home() {
             </div>
           </div>
         )
-  ))}
+  )}
 </div>
 
     </div>
